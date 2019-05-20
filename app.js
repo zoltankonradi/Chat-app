adaca5a8a63703fdf553cccadce5f8ee676c8416
Zoli <zoltan.konradi@gmail.com>
2019-05-20 11:19:16 +0200

    Server side socket connection handler added.

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+// server.listen(process.env.PORT || 8080);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res, next) {
+console.log('Server running...');
+
+app.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
+});
+
+//Connect
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(`Number of sockets connected: ${connections.length}`);
+    //Disconnect
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Socket disconnected.');
 });
 
 module.exports = app;
